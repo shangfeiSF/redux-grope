b0208366925e09fbc6879e3495f5f2948bbf8443
shangfeiSF <tony20100125@126.com>
2016-10-10 16:24:36 +0800

    format the first example(01.counter-vanilla)

--- main/01.counter-vanilla/src/index.js
+++ main/01.counter-vanilla/src/index.js
@@ -1,50 +1,106 @@
 import {createStore} from 'redux'
 
-var store = createStore(counter)
-var valueEl = document.getElementById('value')
+class App {
+  constructor() {
+    this.nodes = {}
 
-function counter(state, action) {
-  if (typeof state === 'undefined') {
-    return 0
-  }
-
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    default:
-      return state
-  }
-}
-
-function render() {
-  valueEl.innerHTML = store.getState().toString()
-}
-
-render()
-store.subscribe(render)
-
-document.getElementById('increment')
-  .addEventListener('click', function () {
-    store.dispatch({type: 'INCREMENT'})
-  })
-
-document.getElementById('decrement')
-  .addEventListener('click', function () {
-    store.dispatch({type: 'DECREMENT'})
-  })
-
-document.getElementById('incrementIfOdd')
-  .addEventListener('click', function () {
-    if (store.getState() % 2 !== 0) {
-      store.dispatch({type: 'INCREMENT'})
+    this.types = {
+      in: 'INCREMENT',
+      de: 'DECREMENT'
     }
-  })
 
-document.getElementById('incrementAsync')
-  .addEventListener('click', function () {
-    setTimeout(function () {
-      store.dispatch({type: 'INCREMENT'})
-    }, 1000)
-  })
+    this.store = null
+  }
+
+  reducers(state, action) {
+    if (typeof state === 'undefined') {
+      return 0
+    }
+
+    switch (action.type) {
+      case this.types.in:
+        return state + 1
+      case this.types.de:
+        return state - 1
+      default:
+        return state
+    }
+  }
+
+  boot() {
+    let self = this
+    let nodes = this.nodes
+
+    let $ = function (id) {
+      return document.getElementById(id)
+    }
+
+    nodes.times = $('times')
+    nodes.increase = $('increase')
+    nodes.decrease = $('decrease')
+    nodes.increaseIfOdd = $('increaseIfOdd')
+    nodes.increaseAsync = $('increaseAsync')
+
+    /*
+     * Redux createStore with reducers
+     * and subscribe the dispatch actions
+     * */
+    this.store = createStore(self.reducers.bind(self))
+    this.store.subscribe(function () {
+      self.render()
+    })
+  }
+
+  render() {
+    let nodes = this.nodes
+    let store = this.store
+
+    nodes.times.innerHTML = store.getState().toString()
+  }
+
+  bind() {
+    let self = this
+    let nodes = this.nodes
+    let store = this.store
+
+    let $ = function (node, type, listener) {
+      node.addEventListener(type, listener)
+    }
+
+    $(nodes.increase, 'click', function () {
+      store.dispatch({
+        type: self.types.in
+      })
+    })
+
+    $(nodes.decrease, 'click', function () {
+      store.dispatch({
+        type: self.types.de
+      })
+    })
+
+    $(nodes.increaseIfOdd, 'click', function () {
+      if (store.getState() % 2 !== 0) {
+        store.dispatch({
+          type: self.types.in
+        })
+      }
+    })
+
+    $(nodes.increaseAsync, 'click', function () {
+      setTimeout(function () {
+        store.dispatch({
+          type: self.types.in
+        })
+      }, 1000)
+    })
+  }
+
+  init() {
+    this.boot()
+    this.render()
+    this.bind()
+  }
+}
+
+new App().init()
