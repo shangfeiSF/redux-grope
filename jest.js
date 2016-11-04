322d77e1aadaa1a7f04a1bc0448b21d01657c3a3
shangfeiSF <tony20100125@126.com>
2016-11-04 14:31:29 +0800

    support regexp in jest.js to test multiple jest-files once

--- jest.js
+++ jest.js
@@ -8,45 +8,63 @@ var Promise = require('bluebird')
 Promise.promisifyAll(fs)
 
 var options = nopt({
-  'path': String
+  'bail': Boolean,
+  'verbose': Boolean,
+  'path': String,
+  'regexp': String
 }, {
+  'b': ['--bail'],
+  'b1': ['--bail', 'true'],
+  'v': ['--verbose'],
+  'v1': ['--verbose', 'true'],
   'p': ['--path'],
+  'r': ['--regexp']
 }, process.argv, 2)
 
 var jestConfigFile = path.join(__dirname, 'config.jest.json')
 
 fs.statAsync(jestConfigFile)
   .then(
-    function () {
+    function (states) {
       return fs.readFileAsync(jestConfigFile, {encoding: 'utf-8'})
         .then(function (data) {
           return {
             existed: true,
+            states: states,
             data: JSON.parse(data)
           }
         })
     },
-    function () {
+    function (errors) {
       return fs.openAsync(jestConfigFile, 'w')
         .then(function (fd) {
           return {
             existed: false,
+            errors: errors,
             fd: fd
           }
         })
     }
   )
   .then(function (spec) {
-    var testRegex = options.path.length ? options.path.replace(/\./g, '\.') + '$' : '(/tests/.*|\\.(test|spec))\\.(js|jsx)$'
+    var testRegex = '(/tests/.*|\\.(test|spec))\\.(js|jsx)$'
+
+    if (options.path && options.path.length) {
+      testRegex = options.path.replace(/\./g, '\.') + '$'
+    }
+    else if (options.regexp && options.regexp.length) {
+      testRegex = options.regexp
+    }
+
+    var json = {
+      testRegex: testRegex,
+      bail: options.bail === undefined ? false : options.bail,
+      verbose: options.verbose === undefined ? false : options.verbose
+    }
 
     if (spec.existed) {
-      spec.data.testRegex = testRegex
-
-      return fs.writeFileAsync(jestConfigFile, JSON.stringify(spec.data, null, 2), {encoding: 'utf-8'})
-    }
-    else {
-      return fs.writeAsync(spec.fd, JSON.stringify({
-        testRegex: testRegex
-      }, null, 2))
+      return fs.writeFileAsync(jestConfigFile, JSON.stringify(json, null, 2), {encoding: 'utf-8'})
+    } else {
+      return fs.writeAsync(spec.fd, JSON.stringify(json, null, 2))
     }
   })
