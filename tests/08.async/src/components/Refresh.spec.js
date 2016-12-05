2bc14c37314bad33a739745ffc308359c8c6036a
shangfeiSF <tony20100125@126.com>
2016-12-05 11:17:43 +0800

    finish to test 08.async/components/Refresh.js

--- tests/08.async/src/components/Refresh.spec.js
+++ tests/08.async/src/components/Refresh.spec.js
@@ -1,14 +1,85 @@
 import React from 'react'
-import {shallow} from 'enzyme'
 import {mount} from 'enzyme'
 import TestUtils from 'react-addons-test-utils'
 
+import Themes from '../../../../main/08.async/src/constants/Themes'
 import Refresh from '../../../../main/08.async/src/components/Refresh'
 
-const setup = () => {
+const setup = propOverrides => {
+  const props = Object.assign({
+    selected: Themes[0],
+
+    lastUpdated: '2016-12-05T12:34:56.789Z',
+
+    isFetching: false,
+
+    asyncActions: {
+      fetchIfNeed: jest.fn()
+    },
+
+    syncActions: {
+      select: jest.fn(),
+      add: jest.fn(),
+      request: jest.fn(),
+      receive: jest.fn(),
+      refresh: jest.fn()
+    },
+  }, propOverrides)
+
+  const renderer = TestUtils.createRenderer()
+  renderer.render(
+    <Refresh {...props} />
+  )
+
+  const output = renderer.getRenderOutput()
+
+  return {
+    props: props,
+    output: output,
+    renderer: renderer
+  }
 }
 
 describe('Refresh component', () => {
-  it('should display correctly', () => {
+  it('should display correctly when `lastUpdated` existed and `isFetching` is false', () => {
+    const {output, props} = setup()
+
+    expect(output.type).toBe('p')
+
+    const [span, ,a] = output.props.children
+
+    expect(span.type).toBe('span')
+    expect(span.props.children).toEqual([
+      'Last updated at ',
+      `${new Date(props.lastUpdated).toLocaleTimeString()}`,
+      '.'
+    ])
+
+    expect(a.type).toBe('a')
+    expect(a.props.children).toBe('Refresh')
+  })
+
+  it('should display correctly when `lastUpdated` not existed and `isFetching` is true', () => {
+    const {output} = setup({
+      lastUpdated: '',
+
+      isFetching: true
+    })
+
+    expect(output.type).toBe('p')
+    expect(output.props.children).toEqual([null, ' ', null])
+  })
+
+  it('should call `syncActions.refresh` and `asyncActions.fetchIfNeed` when click the refresh button', () => {
+    const {props} = setup()
+
+    const output = mount(<Refresh {...props}></Refresh>)
+
+    output.find('a').simulate('click', {
+      preventDefault: jest.fn()
+    })
+
+    expect(props.syncActions.refresh).toBeCalledWith(props.selected)
+    expect(props.asyncActions.fetchIfNeed).toBeCalledWith(props.selected)
   })
 })
