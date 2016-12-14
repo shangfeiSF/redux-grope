3dcf8180c551011d45e21b09a4108cb6af41c6ca
shangfeiSF <tony20100125@126.com>
2016-12-14 23:36:35 +0800

    completed to test routes/env

---
+++ tests/10.real-world/src/routes/env/RootContainer.dev.spec.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import TestUtils from 'react-addons-test-utils'
+
+import {Provider} from 'react-redux'
+import {Router} from 'react-router'
+
+import routes from '../../../../../main/10.real-world/src/routes/routes'
+import Root from '../../../../../main/10.real-world/src/routes/env/RootContainer.dev'
+import DevTools from '../../../../../main/10.real-world/src/uitls/DevTools'
+
+const setup = () => {
+  const props = {
+    store: {
+      entities: {
+        'facebook/react': {
+          id: 10270250,
+          name: "react",
+          fullName: "facebook/react",
+          owner: "facebook",
+        }
+      },
+      subscribe: jest.fn(),
+      dispatch: jest.fn(),
+      getState: jest.fn()
+    },
+    history: {
+      go: jest.fn()
+    },
+  }
+
+  const renderer = TestUtils.createRenderer()
+  renderer.render(
+    <Root {...props} />
+  )
+
+  const output = renderer.getRenderOutput()
+
+  return {
+    props: props,
+    output: output,
+    renderer: renderer
+  }
+}
+
+describe('RootContainer.dev', () => {
+  it('should render correctly', () => {
+    const {output, props} = setup()
+    const div = output.props.children
+
+    expect(output.type).toBe(Provider)
+    expect(output.props.store).toBe(props.store)
+
+    expect(div.type).toBe('div')
+
+    const [p, router, devTools] = div.props.children
+
+    expect(p.props.children).toEqual('Hide DevTools with Ctrl+H.')
+
+    expect(router.type).toBe(Router)
+    expect(router.props.history).toBe(props.history)
+    expect(router.props.routes).toBe(routes)
+
+    expect(devTools.type).toBe(DevTools)
+  })
+})
---
+++ tests/10.real-world/src/routes/env/RootContainer.prod.spec.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import TestUtils from 'react-addons-test-utils'
+
+import {Provider} from 'react-redux'
+import {Router} from 'react-router'
+
+import routes from '../../../../../main/10.real-world/src/routes/routes'
+import Root from '../../../../../main/10.real-world/src/routes/env/RootContainer.prod'
+
+const setup = () => {
+  const props = {
+    store: {
+      entities: {
+        'facebook/react': {
+          id: 10270250,
+          name: "react",
+          fullName: "facebook/react",
+          owner: "facebook",
+        }
+      },
+      subscribe: jest.fn(),
+      dispatch: jest.fn(),
+      getState: jest.fn()
+    },
+    history: {
+      go: jest.fn()
+    },
+  }
+
+  const renderer = TestUtils.createRenderer()
+  renderer.render(
+    <Root {...props} />
+  )
+
+  const output = renderer.getRenderOutput()
+
+  return {
+    props: props,
+    output: output,
+    renderer: renderer
+  }
+}
+
+describe('RootContainer.prod', () => {
+  it('should render correctly', () => {
+    const {output, props} = setup()
+    const router = output.props.children
+
+    expect(output.type).toBe(Provider)
+    expect(output.props.store).toBe(props.store)
+
+    expect(router.type).toBe(Router)
+
+    expect(router.props.history).toBe(props.history)
+    expect(router.props.routes).toBe(routes)
+  })
+})
