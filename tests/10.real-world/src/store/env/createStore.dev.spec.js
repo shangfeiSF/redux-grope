ba164189bf6aa123cbe206693ac1b63f24e65b9d
shangfeiSF <tony20100125@126.com>
2016-12-16 01:02:11 +0800

    can pass createStore.dev.spec.js but there is some loggers unnecessary which should be hidden when run testing

--- tests/10.real-world/src/store/env/createStore.dev.spec.js
+++ tests/10.real-world/src/store/env/createStore.dev.spec.js
@@ -1,4 +1,23 @@
+import {normalize} from 'normalizr'
+import {camelizeKeys} from 'humps'
 import {bindActionCreators} from 'redux'
+
+import {
+  repo as repoJson,
+  response as repoReponse
+} from '../mocks/middleware/modules/repoJson.mock'
+import {
+  user as userJson,
+  response as userReponse
+} from '../mocks/middleware/modules/userJson.mock'
+import {
+  starred as starredJson,
+  response as starredReponse
+} from '../mocks/middleware/modules/starredJson.mock'
+import {
+  stargazers as stargazersJson,
+  response as stargazersReponse
+} from '../mocks/middleware/modules/stargazersJson.mock'
 
 import {mocks} from '../mocks/middleware/modules/utils'
 
@@ -10,6 +29,7 @@ import createStore from '../../../../../main/10.real-world/src/store/env/createS
 
 import {routerReducer as routing} from 'react-router-redux'
 
+import {Schemas} from '../../../../../main/10.real-world/src/middleware/schema'
 import {loadRepo, loadStargazers} from '../../../../../main/10.real-world/src/actions/repoPageThunkActions'
 import {loadUser, loadStarred} from '../../../../../main/10.real-world/src/actions/userPageThunkActions'
 
@@ -37,12 +57,30 @@ describe('createStore.dev', () => {
 
     await loadRepoBinded(fullName, ['description'])
     // store.dispatch(loadStargazers(fullName))
-    //
+
     // store.dispatch(loadUser(login, ['name']))
     // store.dispatch(loadStarred(login))
 
-    let expectedValue = null
+    let camelizedJson = camelizeKeys(repoJson)
+    let normalizedJson = normalize(camelizedJson, Schemas.REPO)
 
-    //expect(store.getState()).toEqual(expectedValue)
+    let extraData = {nextPageUrl: mocks.getNextPageUrl(userReponse)}
+
+    //Object.assign({}, normalizedJson, extraData)
+
+    let expectedValue = {
+      routing: routing,
+      entities: {
+        users: normalizedJson.entities.users,
+        repos: normalizedJson.entities.repos
+      },
+      pagination: {
+        starredByUser: {},
+        stargazersByRepo: {}
+      },
+      errorMessage: null
+    }
+
+    expect(store.getState()).toEqual(expectedValue)
   })
 })
