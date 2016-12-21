4564c7725bdb7e0782ebe1cb4f4b356978a233a9
shangfeiSF <tony20100125@126.com>
2016-12-21 16:40:13 +0800

    completed the RepoPage.spec.js and UserPage.spec.js

--- tests/10.real-world/src/components/RepoPage.spec.js
+++ tests/10.real-world/src/components/RepoPage.spec.js
@@ -1,4 +1,174 @@
+import React from 'react'
+import sinon from 'sinon'
+import {mount} from 'enzyme'
+import TestUtils from 'react-addons-test-utils'
+
+import Repo from '../../../../main/10.real-world/src/components/modules/Repo'
+import List from '../../../../main/10.real-world/src/components/modules/List'
+
+import RepoPage from '../../../../main/10.real-world/src/components/RepoPage'
+
+const setup = propOverrides => {
+  const props = Object.assign({
+    repo: {
+      id: 10270250,
+      name: 'react',
+      full_name: 'facebook/react'
+    },
+    owner: {
+      login: 'facebook',
+      id: 69631,
+      type: 'Organization'
+    },
+
+    name: 'react',
+    fullName: 'facebook/react',
+
+    stargazersPagination: {
+      ids: [1, 2, 3, 4, 5],
+      pageCount: 1,
+      isFetching: false,
+      nextPageUrl: 'This is nextPageUrl'
+    },
+    stargazers: [],
+
+    loadRepo: jest.fn(),
+
+    loadStargazers: jest.fn()
+  }, propOverrides)
+
+  const renderer = TestUtils.createRenderer()
+  renderer.render(
+    <RepoPage {...props} />
+  )
+
+  const output = renderer.getRenderOutput()
+
+  return {
+    props: props,
+    output: output,
+    renderer: renderer
+  }
+}
+
 describe('RepoPage', () => {
-  it('', () => {
+  it('should display correctly when repo or owner is undefined', () => {
+    const {output, props} = setup({
+      repo: undefined
+    })
+
+    const i = output.props.children
+
+    expect(output.type).toBe('h1')
+    expect(i.type).toBe('i')
+
+    expect(i.props.children).toEqual(['Loading ', props.name, 'details...'])
+  })
+
+  it('should display correctly', () => {
+    const {output, props} = setup()
+
+    expect(output.type).toBe('div')
+
+    const [repo, list] = output.props.children
+
+    expect(repo.type).toBe(Repo)
+    expect(list.type).toBe(List)
+
+    expect(repo.props.repo).toEqual(props.repo)
+    expect(repo.props.owner).toEqual(props.owner)
+
+    expect(list.props.items).toEqual(props.stargazers)
+    expect(list.props.loadingLabel).toEqual(`Loading stargazers of ${props.name}...`)
+    expect(list.props.ids).toEqual(props.stargazersPagination.ids)
+    expect(list.props.pageCount).toEqual(props.stargazersPagination.pageCount)
+    expect(list.props.isFetching).toEqual(props.stargazersPagination.isFetching)
+    expect(list.props.nextPageUrl).toEqual(props.stargazersPagination.nextPageUrl)
+  })
+
+  it('should loadData on `componentWillMount`', () => {
+    const {props} = setup()
+
+    const loadData = sinon.spy(props => {
+      const {fullName} = props
+
+      props.loadRepo(fullName, ['description'])
+      props.loadStargazers(fullName)
+    })
+
+    const spy = sinon.stub(RepoPage.prototype, 'componentWillMount', () => {
+      loadData(props)
+    })
+
+    const output = mount(<RepoPage {...props}></RepoPage>)
+
+    expect(spy.calledOnce).toEqual(true)
+    expect(loadData.calledOnce).toEqual(true)
+    expect(loadData.calledWith(props)).toEqual(true)
+
+    RepoPage.prototype.componentWillMount.restore()
+  })
+
+  it('should call loadData on `componentWillReceiveProps` if received a new fullname', () => {
+    const {props} = setup()
+
+    const loadData = sinon.spy(props => {
+      const {fullName} = props
+
+      props.loadRepo(fullName, ['description'])
+      props.loadStargazers(fullName)
+    })
+
+    const spy = sinon.stub(RepoPage.prototype, 'componentWillReceiveProps', nextProps => {
+      if (nextProps.fullName !== props.fullName) {
+        loadData(nextProps)
+      }
+    })
+
+    const output = mount(<RepoPage {...props}></RepoPage>)
+
+    expect(spy.calledOnce).toEqual(false)
+
+    const {props: nextProps} = setup({
+      fullName: 'facebook/jest'
+    })
+    output.setProps(nextProps)
+
+    expect(spy.calledOnce).toEqual(true)
+    expect(loadData.calledOnce).toEqual(true)
+    expect(loadData.calledWith(nextProps)).toEqual(true)
+
+    RepoPage.prototype.componentWillReceiveProps.restore()
+  })
+
+  it('should not call loadData on `componentWillReceiveProps` if received a old fullname', () => {
+    const {props} = setup()
+
+    const loadData = sinon.spy(props => {
+      const {fullName} = props
+
+      props.loadRepo(fullName, ['description'])
+      props.loadStargazers(fullName)
+    })
+
+    const spy = sinon.stub(RepoPage.prototype, 'componentWillReceiveProps', nextProps => {
+      if (nextProps.fullName !== props.fullName) {
+        loadData(nextProps)
+      }
+    })
+
+    const output = mount(<RepoPage {...props}></RepoPage>)
+
+    expect(spy.calledOnce).toEqual(false)
+
+    const {props: nextProps} = setup({
+      fullName: props.fullName
+    })
+    output.setProps(nextProps)
+
+    expect(spy.calledOnce).toEqual(true)
+    expect(loadData.calledOnce).toEqual(false)
+
+    RepoPage.prototype.componentWillReceiveProps.restore()
   })
 })
--- tests/10.real-world/src/components/UserPage.spec.js
+++ tests/10.real-world/src/components/UserPage.spec.js
@@ -1,4 +1,187 @@
+import zip from 'lodash/zip'
+
+import React from 'react'
+import sinon from 'sinon'
+import {mount} from 'enzyme'
+import TestUtils from 'react-addons-test-utils'
+
+import User from '../../../../main/10.real-world/src/components/modules/User'
+import List from '../../../../main/10.real-world/src/components/modules/List'
+
+import UserPage from '../../../../main/10.real-world/src/components/UserPage'
+
+const setup = propOverrides => {
+  const props = Object.assign({
+    login: 'tj',
+    user: {
+      login: "tj",
+      id: 25254,
+      avatarUrl: 'https://avatars.githubusercontent.com/u/25254?v=3',
+      type: "User"
+    },
+
+    starredPagination: {
+      ids: [1, 2, 3, 4, 5],
+      pageCount: 1,
+      isFetching: false,
+      nextPageUrl: 'This is nextPageUrl'
+    },
+
+    starredRepos: [{
+      id: 26505240,
+      fullName: 'godoctor/godoctor',
+      name: 'godoctor',
+      description: 'Go Doctor - The Golang Refactoring Engine'
+    }, {
+      id: 65794292,
+      fullName: 'styled-components/styled-components',
+      name: 'styled-components',
+      description: 'Visual primitives for the component age 💅'
+    }],
+    starredRepoOwners: [{
+      id: 9682935,
+      login: 'godoctor'
+    }, {
+      id: 20658825,
+      login: 'styled-components'
+    }],
+
+    loadUser: jest.fn(),
+
+    loadStarred: jest.fn()
+  }, propOverrides)
+
+  const renderer = TestUtils.createRenderer()
+  renderer.render(
+    <UserPage {...props} />
+  )
+
+  const output = renderer.getRenderOutput()
+
+  return {
+    props: props,
+    output: output,
+    renderer: renderer
+  }
+}
+
 describe('UserPage', () => {
-  it('', () => {
+  it('should display correctly when user is undefined', () => {
+    const {output, props} = setup({
+      user: undefined
+    })
+
+    const i = output.props.children
+
+    expect(output.type).toBe('h1')
+    expect(i.type).toBe('i')
+
+    expect(i.props.children).toEqual(['Loading ', props.login, "'s profile..."])
+  })
+
+  it('should display correctly', () => {
+    const {output, props} = setup()
+
+    expect(output.type).toBe('div')
+
+    const [user, list] = output.props.children
+
+    expect(user.type).toBe(User)
+    expect(list.type).toBe(List)
+
+    expect(user.props.user).toEqual(props.user)
+
+    expect(list.props.items).toEqual(zip(props.starredRepos, props.starredRepoOwners))
+    expect(list.props.loadingLabel).toEqual(`Loading ${props.login}'s starred...`)
+    expect(list.props.ids).toEqual(props.starredPagination.ids)
+    expect(list.props.pageCount).toEqual(props.starredPagination.pageCount)
+    expect(list.props.isFetching).toEqual(props.starredPagination.isFetching)
+    expect(list.props.nextPageUrl).toEqual(props.starredPagination.nextPageUrl)
+  })
+
+  it('should loadData on `componentWillMount`', () => {
+    const {props} = setup()
+
+    const loadData = sinon.spy(props => {
+      const {login} = props
+
+      props.loadUser(login, ['name'])
+      props.loadStarred(login)
+    })
+
+    const spy = sinon.stub(UserPage.prototype, 'componentWillMount', () => {
+      loadData(props)
+    })
+
+    const output = mount(<UserPage {...props}></UserPage>)
+
+    expect(spy.calledOnce).toEqual(true)
+    expect(loadData.calledOnce).toEqual(true)
+    expect(loadData.calledWith(props)).toEqual(true)
+
+    UserPage.prototype.componentWillMount.restore()
+  })
+
+  it('should call loadData on `componentWillReceiveProps` if received a new login', () => {
+    const {props} = setup()
+
+    const loadData = sinon.spy(props => {
+      const {login} = props
+
+      props.loadUser(login, ['name'])
+      props.loadStarred(login)
+    })
+
+    const spy = sinon.stub(UserPage.prototype, 'componentWillReceiveProps', nextProps => {
+      if (nextProps.login !== props.login) {
+        loadData(nextProps)
+      }
+    })
+
+    const output = mount(<UserPage {...props}></UserPage>)
+
+    expect(spy.calledOnce).toEqual(false)
+
+    const {props: nextProps} = setup({
+      login: 'shangfeiSF'
+    })
+    output.setProps(nextProps)
+
+    expect(spy.calledOnce).toEqual(true)
+    expect(loadData.calledOnce).toEqual(true)
+    expect(loadData.calledWith(nextProps)).toEqual(true)
+
+    UserPage.prototype.componentWillReceiveProps.restore()
+  })
+
+  it('should not call loadData on `componentWillReceiveProps` if received a old login', () => {
+    const {props} = setup()
+
+    const loadData = sinon.spy(props => {
+      const {login} = props
+
+      props.loadUser(login, ['name'])
+      props.loadStarred(login)
+    })
+
+    const spy = sinon.stub(UserPage.prototype, 'componentWillReceiveProps', nextProps => {
+      if (nextProps.login !== props.login) {
+        loadData(nextProps)
+      }
+    })
+
+    const output = mount(<UserPage {...props}></UserPage>)
+
+    expect(spy.calledOnce).toEqual(false)
+
+    const {props: nextProps} = setup({
+      login: props.login
+    })
+    output.setProps(nextProps)
+
+    expect(spy.calledOnce).toEqual(true)
+    expect(loadData.calledOnce).toEqual(false)
+
+    UserPage.prototype.componentWillReceiveProps.restore()
   })
 })
