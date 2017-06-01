ac2edc34a77744d3838c30b646b686d035736def
shangfeiSF <tony20100125@126.com>
2017-06-02 00:52:42 +0800

    add models to 10

---
+++ main/10.real-world/src/models/index.js
@@ -0,0 +1,17 @@
+import Immutable from 'immutable'
+import {LOCATION_BEFORE_TRANSITIONS} from '../constants/RouterRedux'
+
+export const entitiesInitialState = Immutable.fromJS({
+  users: {},
+  repos: {}
+})
+
+export const errorMessageInitialState = Immutable.fromJS(null)
+
+export const starredByUserInitialState = Immutable.fromJS({})
+
+export const stargazersByRepoInitialState = Immutable.fromJS({})
+
+export const routingInitialState = Immutable.fromJS({
+  [LOCATION_BEFORE_TRANSITIONS]: null
+})
